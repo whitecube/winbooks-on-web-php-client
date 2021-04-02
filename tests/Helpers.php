cb91c7493aeecdd4d19cae1b5e10ab4d206ed30a
Toon Van den Bos <toon@whitecube.be>
2021-04-02 17:26:34 +0200

    Fixed API authentication when environment variables are already defined

--- tests/Helpers.php
+++ tests/Helpers.php
@@ -2,17 +2,19 @@
 
 namespace Tests;
 
-
 function credentials() {
-    if(! isset($_ENV['WOW_TEST_EMAIL']) || ! isset($_ENV['WOW_TEST_EXCHANGE_TOKEN'])) {
+    $email = getenv('WOW_TEST_EMAIL');
+    $token = getenv('WOW_TEST_EXCHANGE_TOKEN');
+
+    if(! $email || ! $token) {
         $dotenv = \Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
         $dotenv->load();
+
+        $email = $_ENV['WOW_TEST_EMAIL'];
+        $token = $_ENV['WOW_TEST_EXCHANGE_TOKEN'];
     }
 
-    return [
-        $_ENV['WOW_TEST_EMAIL'],
-        $_ENV['WOW_TEST_EXCHANGE_TOKEN']
-    ];
+    return [$email, $token];
 }
 
 function authenticate() {
