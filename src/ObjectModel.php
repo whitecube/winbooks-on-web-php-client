3b70f9b968bc57c6566af46b5aeacd5f34829a80
Toon Van den Bos <toon@whitecube.be>
2021-04-07 14:15:10 +0200

    Added ObjectModel ArrayAccess implementation

--- src/ObjectModel.php
+++ src/ObjectModel.php
@@ -2,21 +2,28 @@
 
 namespace Whitecube\Winbooks;
 
+use ArrayAccess;
 use ReflectionClass;
 use JsonSerializable;
 
-abstract class ObjectModel implements JsonSerializable
+abstract class ObjectModel implements ArrayAccess, JsonSerializable
 {
     /**
      * Container for the model's data
      *
      * @var array
      */
-    protected $data = [];
+    protected $attributes = [];
 
-    public function __construct(array $data = [])
+    /**
+     * Create a new ObjectModel instance
+     *
+     * @param array $attributes
+     * @return void
+     */
+    public function __construct(array $attributes = [])
     {
-        $this->data = $data;
+        $this->attributes = $attributes;
     }
 
     /**
@@ -33,12 +40,12 @@ abstract class ObjectModel implements JsonSerializable
      */
     public function getCode(): ?string
     {
-        if(array_key_exists('Code', $this->data)) {
-            return $this->Code;
+        if($this->has('Code')) {
+            return $this->get('Code');
         }
 
-        if(array_key_exists('Id', $this->data)) {
-            return $this->Id;
+        if($this->has('Id')) {
+            return $this->get('Id');
         }
 
         return null;
@@ -63,29 +70,124 @@ abstract class ObjectModel implements JsonSerializable
      */
     public function getOMS(): string
     {
-        return (new ReflectionClass($this))->getShortName() . 's';
+        return $this->getOM() . 's';
+    }
+
+    /**
+     * Check if a given attribute exists in the model's attributes
+     *
+     * @param string $attribute
+     * @return bool
+     */
+    public function has($attribute): bool
+    {
+        return array_key_exists($attribute, $this->attributes);
+    }
+
+    /**
+     * ArrayAccess' has alias
+     *
+     * @param string $attribute
+     * @return bool
+     */
+    public function offsetExists($attribute)
+    {
+        return $this->has($attribute);
     }
 
     /**
      * Set a value on the model
      *
-     * @param string $name
-     * @param $value
+     * @param string $attribute
+     * @param mixed $value
+     * @return void
      */
-    public function __set(string $name, $value)
+    public function set(string $attribute, $value)
     {
-        $this->data[$name] = $value;
+        $this->attributes[$attribute] = $value;
+    }
+
+    /**
+     * Magically set a value on the model
+     *
+     * @param string $attribute
+     * @param mixed $value
+     * @return void
+     */
+    public function __set(string $attribute, $value)
+    {
+        $this->set($attribute, $value);
+    }
+
+    /**
+     * ArrayAccess' set alias
+     *
+     * @param string $attribute
+     * @param mixed $value
+     * @return void
+     */
+    public function offsetSet($attribute, $value)
+    {
+        $this->set($attribute, $value);
     }
 
     /**
      * Get a value on the model
      *
-     * @param string $name
+     * @param string $attribute
      * @return mixed
      */
-    public function __get(string $name)
+    public function get(string $attribute)
     {
-        return $this->data[$name] ?? null;
+        return $this->attributes[$attribute] ?? null;
+    }
+
+    /**
+     * Magically get a value on the model
+     *
+     * @param string $attribute
+     * @return mixed
+     */
+    public function __get(string $attribute)
+    {
+        return $this->get($attribute);
+    }
+
+    /**
+     * ArrayAccess' get alias
+     *
+     * @param string $attribute
+     * @return mixed
+     */
+    public function offsetGet($attribute)
+    {
+        return $this->get($attribute);
+    }
+
+    /**
+     * Unset a value on the model
+     *
+     * @param string $attribute
+     * @return void
+     */
+    public function remove(string $attribute)
+    {
+        if(! $this->has($attribute)) {
+            return;
+        }
+
+        unset($this->attributes[$attribute]);
+    }
+
+    /**
+     * ArrayAccess' remove alias
+     *
+     * @param string $attribute
+     * @return void
+     */
+    public function offsetUnset($attribute)
+    {
+        $this->remove($attribute);
     }
 
     /**
@@ -95,6 +197,9 @@ abstract class ObjectModel implements JsonSerializable
      */
     public function jsonSerialize(): array
     {
-        return array_merge(['$type' => $this->getType()], $this->data);
+        return array_merge(
+            ['$type' => $this->getType()],
+            $this->attributes
+        );
     }
 }
--- tests/Feature/Models/ModelTest.php
+++ tests/Feature/Models/ModelTest.php
@@ -4,31 +4,83 @@ use Whitecube\Winbooks\Winbooks;
 use Whitecube\Winbooks\Models\Customer;
 use function Tests\authenticate;
 
-
-test('a model can accept properties dynamically', function() {
-    $customer = new Customer();
-    $customer->foobar = 'baz';
-
-    expect($customer->foobar)->toBe('baz');
-});
-
-
-test('a model can be serialized into the correct JSON structure', function() {
-    $customer = new Customer();
-    $customer->foobar = 'baz';
-    $encoded = json_encode($customer);
-
-    $this->assertStringContainsString('"foobar":"baz"', $encoded);
-    $this->assertStringContainsString('"$type":"Winbooks.TORM.OM.Customer, Winbooks.TORM.OM"', $encoded);
-});
-
-
 test('a model can accept values in its constructor', function() {
     $customer = new Customer(['foo' => 'bar']);
 
     expect($customer->foo)->toBe('bar');
 });
 
+test('a model can set properties methodologically', function() {
+    $customer = new Customer();
+    $customer->set('foo', 'bar');
+
+    expect($customer->foo)->toBe('bar');
+});
+
+test('a model can set properties dynamically', function() {
+    $customer = new Customer();
+    $customer->foo = 'bar';
+
+    expect($customer->foo)->toBe('bar');
+});
+
+test('a model can set properties as an array', function() {
+    $customer = new Customer();
+    $customer['foo'] = 'bar';
+
+    expect($customer->foo)->toBe('bar');
+});
+
+test('a model can return properties methodologically', function() {
+    $customer = new Customer(['foo' => 'bar']);
+
+    expect($customer->get('foo'))->toBe('bar');
+});
+
+test('a model can return properties dynamically', function() {
+    $customer = new Customer(['foo' => 'bar']);
+
+    expect($customer->foo)->toBe('bar');
+});
+
+test('a model can return properties as an array', function() {
+    $customer = new Customer(['foo' => 'bar']);
+
+    expect($customer['foo'])->toBe('bar');
+});
+
+test('a model can check property existence', function() {
+    $customer = new Customer(['foo' => 'bar']);
+
+    expect($customer->has('bar'))->toBeFalse();
+    expect($customer->has('foo'))->toBeTrue();
+});
+
+test('a model can remove properties methodologically', function() {
+    $customer = new Customer(['foo' => 'bar', 'baz' => 'test']);
+
+    $customer->remove('baz');
+
+    expect($customer->has('baz'))->toBeFalse();
+    expect($customer->has('foo'))->toBeTrue();
+});
+
+test('a model can remove properties as an array', function() {
+    $customer = new Customer(['foo' => 'bar', 'baz' => 'test']);
+
+    unset($customer['baz']);
+
+    expect($customer->has('baz'))->toBeFalse();
+    expect($customer->has('foo'))->toBeTrue();
+});
+
+test('a model can be serialized into the correct JSON structure', function() {
+    $customer = new Customer(['foo' => 'bar']);
+    $encoded = json_encode($customer);
+
+    $this->assertStringContainsString('"foo":"bar"', $encoded);
+    $this->assertStringContainsString('"$type":"Winbooks.TORM.OM.Customer, Winbooks.TORM.OM"', $encoded);
+});
 
 test('a model can return its Code or its Id as a fallback', function() {
     $alice = new Customer(['Code' => 'ALICE']);
