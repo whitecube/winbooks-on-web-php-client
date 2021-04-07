0c717c456b7592b545429437da8360951e63f961
Toon Van den Bos <toon@whitecube.be>
2021-04-07 12:15:37 +0200

    Added a few more Object Models

---
+++ src/Models/Logistics/JournalLog.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Whitecube\Winbooks\Models\Logistics;
+
+use Whitecube\Winbooks\ObjectModel;
+
+class JournalLog extends ObjectModel
+{
+
+    public function getType(): string
+    {
+        return 'Winbooks.TORM.OM.Logistics.JournalLog, Winbooks.TORM.OM';
+    }
+
+}
