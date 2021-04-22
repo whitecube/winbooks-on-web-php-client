485f39c1464f2d588ea9e5706bc67e2155834655
Toon Van den Bos <toon@whitecube.be>
2021-04-22 11:36:07 +0200

    Fixed ThirdAddress definition

--- src/Models/ThirdAddress.php
+++ src/Models/ThirdAddress.php
@@ -6,10 +6,24 @@ use Whitecube\Winbooks\ObjectModel;
 
 class ThirdAddress extends ObjectModel
 {
+    /**
+     * Get the object model's singular name
+     *
+     * @return string
+     * @throws \ReflectionException
+     */
+    public function getOM(): string
+    {
+        return 'Third_Address';
+    }
 
+    /**
+     * Get the object model's $type string
+     *
+     * @return string
+     */
     public function getType(): string
     {
         return 'Winbooks.TORM.OM.Third_Address, Winbooks.TORM.OM';
     }
-
 }
